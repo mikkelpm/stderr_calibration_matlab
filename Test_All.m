e7cdc30b9ecaeb052e11fc9a7d9c7e25b7aca392
MattCocci <matthew.cocci@gmail.com>
2021-01-30 14:41:44 -0500

    Add code for solving for worst case optimal W via median regression:

--- Test_All.m
+++ Test_All.m
@@ -9,9 +9,19 @@
   % worst-case variance problems
   addpath Main/
 
+  % Directory with supporting code to help out the functions in Main
+  addpath Supporting/
+
 
 %% Testing
 
 % Test the program that solves the SDP program max x'Vx for V incomplete
-%Test_SDP(0)   % Do not do speed tests
-Test_SDP(1)  % Do speed test. Takes a long time.
+Test_SDP(0)   % Do not do speed tests
+%Test_SDP(1)  % Do speed test. Takes a long time.
+
+
+% Test the program that solves for the worst-case optimal W matrix when
+% testing a hypothesis about a single parameter using the quantile
+% regression approach approved in the paper. Do this for multiple G
+% matrices.
+Test_ComputeWorstCaseOptimal_Single
